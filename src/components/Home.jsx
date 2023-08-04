1dc1515358148064e7d69444109e51e1fa54cf99
Nahuel <nahuelvenerus@gmail.com>
2023-08-04 03:14:15 -0300

    feat: added about section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,17 @@ import { NavbarName } from "../commons/NavbarName";
 import { IoIosArrowForward } from "react-icons/io";
 import HomeArrow from "../commons/HomeArrow";
 import "../styles/backgroundChanger.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setColor } from "../store/color";
+import { About } from "./About";
 
 const BackgroundChanger = () => {
   const dispatch = useDispatch();
+  const color = useSelector((state) => state.color);
   const [currentBackground, setCurrentBackground] = useState(backgroundImage1);
   const [navbarPosition, setNavbarPosition] = useState(0);
+  const [top, setTop] = useState("10%");
+  const [borderTop, setBorderTop] = useState("-50%");
 
   const handleBackgroundChange = (newBackground) => {
     setCurrentBackground(newBackground);
@@ -22,7 +26,7 @@ const BackgroundChanger = () => {
 
   useEffect(() => {
     dispatch(setColor("pink"));
-  }, []);
+  }, [dispatch]);
 
   return (
     <div
@@ -35,12 +39,18 @@ const BackgroundChanger = () => {
         width: "100vw",
       }}
     >
-      {currentBackground != backgroundImage1 ? (
+      <div className="top-section" style={{ top: top }}>
+        <div className="title">NAHUEL VENERUS</div>
+        <div className="subtitle">FULL STACK DEVELOPER</div>
+      </div>
+      {currentBackground !== backgroundImage1 ? (
         <div
           onClick={() => {
             dispatch(setColor("pink"));
             handleBackgroundChange(backgroundImage1);
             setNavbarPosition(0);
+            setTop("10%");
+            setBorderTop("-50%");
           }}
         >
           <HomeArrow />
@@ -49,7 +59,13 @@ const BackgroundChanger = () => {
         ""
       )}
       <nav className="navbar" style={{ bottom: navbarPosition }}>
-        <div onClick={() => setNavbarPosition("75%")}>
+        <div
+          onClick={() => {
+            setTop("-50%");
+            setNavbarPosition("75%");
+            setBorderTop("60%");
+          }}
+        >
           <NavbarName
             icon={<IoIosArrowForward />}
             image={backgroundImage2}
@@ -59,7 +75,13 @@ const BackgroundChanger = () => {
             currentBackground={currentBackground}
           />
         </div>
-        <div onClick={() => setNavbarPosition("75%")}>
+        <div
+          onClick={() => {
+            setTop("-50%");
+            setNavbarPosition("75%");
+            setBorderTop("60%");
+          }}
+        >
           <NavbarName
             icon={<IoIosArrowForward />}
             image={backgroundImage3}
@@ -69,7 +91,13 @@ const BackgroundChanger = () => {
             currentBackground={currentBackground}
           />
         </div>
-        <div onClick={() => setNavbarPosition("75%")}>
+        <div
+          onClick={() => {
+            setTop("-50%");
+            setNavbarPosition("75%");
+            setBorderTop("60%");
+          }}
+        >
           <NavbarName
             icon={<IoIosArrowForward />}
             image={backgroundImage4}
@@ -79,7 +107,13 @@ const BackgroundChanger = () => {
             currentBackground={currentBackground}
           />
         </div>
-        <div onClick={() => setNavbarPosition("75%")}>
+        <div
+          onClick={() => {
+            setTop("-50%");
+            setNavbarPosition("75%");
+            setBorderTop("60%");
+          }}
+        >
           <NavbarName
             icon={<IoIosArrowForward />}
             image={backgroundImage5}
@@ -90,6 +124,22 @@ const BackgroundChanger = () => {
           />
         </div>
       </nav>
+
+      {currentBackground === backgroundImage2 || backgroundImage1 ? (
+        <div
+          className="border"
+          style={{
+            transition: "1s ease",
+            top: borderTop,
+            color: color,
+            border: `${color} solid 10px`,
+          }}
+        >
+          <About />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
